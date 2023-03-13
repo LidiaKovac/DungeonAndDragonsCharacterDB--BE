74880ac442ee4c9ebb993461c2059afe7ddf19d6
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-03-13 23:37:21 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,11 +12,8 @@ import passive_data from "./services/dnd_data/index"
 import characterRoute from "./services/characters"
 import userRoute from "./services/users"
 
-app.use(cors({
-    exposedHeaders: "token",
-    origin: ["https://heartfelt-pasca-9ed41e.netlify.app", "http://localhost:3000"]
-}))
 app.use(express.json())
+app.use(cors())
 
 // app.use(require("helmet")())
 app.options('*', cors())
