54e0fb2c7f9a82dd16f03bcb4c8d66a21f26ff59
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-03-13 23:35:30 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ import userRoute from "./services/users"
 
 app.use(cors({
     exposedHeaders: "token",
-    origin: "*"
+    origin: ["https://heartfelt-pasca-9ed41e.netlify.app", "http://localhost:3000"]
 }))
 app.use(express.json())
 
