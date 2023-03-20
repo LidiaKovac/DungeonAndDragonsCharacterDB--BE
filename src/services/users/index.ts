48ec5b97b641280fcbf3bfaa0aed022fe72de2b5
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-03-20 21:22:39 +0100

    add logout and create user routes

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -7,6 +7,7 @@ import { compare, hash } from "bcryptjs"
 import User from "../../db/models/user"
 import { calculateProf } from "../../utils"
 import { authMidd, generateJWT } from "../../utils/auth"
+import multer from "multer"
 const { SALT } = process.env
 //admin middleware
 userRoute.get(
@@ -72,14 +73,31 @@ userRoute.get(
 
 userRoute.post(
   "/",
+  multer().fields([
+    { name: "full_name" },
+    { name: "email" },
+    { name: "nickname" },
+    { name: "password" },
+    { name: "password_confirm" },
+  ]),
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
+      if (req.body.password !== req.body.password_confirm)
+        res.status(400).send("Passwords don't match!")
+      const checkUnique = await User.findOne({
+        where: {
+          email: req.body.email
+        }
+      })
+      if(checkUnique) {
+        res.status(400).send("Email is already in our system!")
+      }
       const { full_name, email, nickname } = await User.create({
         ...req.body,
         password: await hash(req.body.password, SALT!),
       })
-      const token = await generateJWT({ full_name, email, nickname })
-      res.header("token").send(token)
+      // const token = await generateJWT({ full_name, email, nickname })
+      res.status(201).send("User created")
     } catch (e) {
       next(e)
     }
